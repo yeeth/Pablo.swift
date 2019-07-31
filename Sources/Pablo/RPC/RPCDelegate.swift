585d4e723bec7307de292ce906d173ec1ef517a6
decanus <dean.eigenmann@icloud.com>
2019-07-31 12:22:18 +0200

    added comments

--- Sources/Pablo/RPC/RPCDelegate.swift
+++ Sources/Pablo/RPC/RPCDelegate.swift
@@ -2,7 +2,18 @@ import Foundation
 
 protocol RPCDelegate {
 
+    /// Tells the client that a new `Epoch` was received.
+    ///
+    /// - Parameters
+    ///     - rpc: The RPC client.
+    ///     - epoch: The new epoch.
     func rpc(_ rpc: RPC, didReceiveEpoch epoch: Epoch)
+
+    /// Tells the client that a new `Slot` was received.
+    ///
+    /// - Parameters
+    ///     - rpc: The RPC client.
+    ///     - slot: The new slot.
     func rpc(_ rpc: RPC, didReceiveSlot slot: Slot)
 
 }
